fc1d0a4e232805566c05f83d87a42b791277a7ca
hkgnp <github.com/hkgnp>
2023-09-20 21:22:55 +0800

    feat: use vite and tui/calendar
    BREAKING CHANGE: use vite and tui/calendar

--- src/features/CalendarApp.tsx
+++ src/features/CalendarApp.tsx
@@ -70,7 +70,7 @@ export const CalendarApp = ({ allEvents }: CalendarProps) => {
           >
             <AiOutlineArrowRight />
           </button>
-          <div className="inline-block relative w-40 ml-2 mt-0.5">
+          <div className="inline-block relative w-40 mt-0.5">
             <select
               className="text-sm block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               onChange={(e) => changeView(e.target.value)}
