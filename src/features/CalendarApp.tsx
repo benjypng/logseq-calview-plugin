70910fb252ed8c7175480704b063a928518a7e8e
hkgnp <github.com/hkgnp>
2023-09-20 17:36:37 +0800

    chore: add nav buttons

--- src/features/CalendarApp.tsx
+++ src/features/CalendarApp.tsx
@@ -5,18 +5,45 @@ import { CalendarProps } from "~/types";
 import { useRef } from "react";
 
 export const CalendarApp = ({ allEvents }: CalendarProps) => {
-  const calendarRef = useRef(null);
-  const test = () => {
-    //@ts-ignore
+  const calendarRef = useRef<Calendar>(null);
+  const navigate = (flag: string) => {
     const calendarInstance = calendarRef.current?.getInstance();
-    calendarInstance.next();
+    if (!calendarInstance) return;
+    switch (flag) {
+      case "previous":
+        calendarInstance.prev();
+        return;
+      case "next":
+        calendarInstance.next();
+        return;
+      default:
+        calendarInstance.today();
+    }
   };
+
   return (
     <div className="flex h-screen justify-end" tabIndex={-1}>
-      <div className="w-96 calApp">
-        <button onClick={test} className="bg-red-900">
-          Test
-        </button>
+      <div className="calApp w-96 bg-white border border-black">
+        <div className="bg-white flex justify-center">
+          <button
+            onClick={() => navigate("previous")}
+            className="w-24 text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => navigate("today")}
+            className="w-24 text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4"
+          >
+            Today
+          </button>
+          <button
+            onClick={() => navigate("next")}
+            className="w-24 text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+          >
+            Next
+          </button>
+        </div>
         <Calendar
           ref={calendarRef}
           isReadOnly={true}
